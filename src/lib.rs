06d998101daeac0d39d85ddc16cddb3ee7423522
Jacob Curley <jcurley@fnal.gov>
2026-01-08 22:59:51 +0000

    Modify data store for query parameters other than strings

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,7 @@
+//! A Rust library providing abstractions for interacting with various data stores in a unified manner.
+//! It defines traits for data values, data rows, parameterized queries, and data stores,
+//! along with a Postgres implementation and test utilities.
+
 use async_trait::async_trait;
 use chrono::{DateTime, Utc};
 use std::{
@@ -5,7 +9,11 @@ use std::{
     fmt::{self, Display, Formatter},
 };
 
+/// Postgres implementation of the traits in this library.
 pub mod postgres;
+
+/// A collection of prebuilt implementations of the traits in this library that are useful for unit tests.
+pub mod test_utils;
 
 /// Custom error type for [`DataStore`] operations
 #[derive(Debug)]
@@ -27,6 +35,9 @@ pub trait DataVal: Send + Sync {
     /// Attempts to decode the value as a [`bool`].
     fn to_bool(self) -> Result<bool, DataStoreError>;
 
+    /// Attempts to decode the value as a [`DateTime<Utc>`].
+    fn to_datetime(self) -> Result<DateTime<Utc>, DataStoreError>;
+
     /// Attempts to decode the value as a [`i8`].
     fn to_i8(self) -> Result<i8, DataStoreError>;
 
@@ -47,9 +58,44 @@ pub trait DataVal: Send + Sync {
 
     /// Attempts to decode the value as a [`String`].
     fn to_string(self) -> Result<String, DataStoreError>;
+}
 
-    /// Attempts to decode the value as a [`DateTime<Utc>`].
-    fn to_datetime(self) -> Result<DateTime<Utc>, DataStoreError>;
+/// Abstraction representing a parameterized query. Exposes a method for binding parameters to the query statement.
+///
+/// It is expected that the query string will have sequential placeholders for the paramterized data.
+/// Example: If passing 5 elements into the query. the query should contain `$1`, `$2`, `$3`, `$4`, and `$5`, and
+/// the bindings should have no fewer than 5 elements (any additional elements beyond 5 will be ignored).
+#[async_trait]
+pub trait ParameterizedQuery<'a, T: DataVal, U: DataRow<T>>: Send + Sync {
+    /// Binds a [`bool`] to the query.
+    fn bind_bool(&'a mut self, parameter: bool) -> Result<(), DataStoreError>;
+
+    /// Binds a [`DateTime<Utc>`] to the query.
+    fn bind_datetime(&'a mut self, parameter: DateTime<Utc>) -> Result<(), DataStoreError>;
+
+    /// Binds a [`i8`] to the query.
+    fn bind_i8(&'a mut self, parameter: i8) -> Result<(), DataStoreError>;
+
+    /// Binds a [`i16`] to the query.
+    fn bind_i16(&'a mut self, parameter: i16) -> Result<(), DataStoreError>;
+
+    /// Binds a [`i32`] to the query.
+    fn bind_i32(&'a mut self, parameter: i32) -> Result<(), DataStoreError>;
+
+    /// Binds a [`i64`] to the query.
+    fn bind_i64(&'a mut self, parameter: i64) -> Result<(), DataStoreError>;
+
+    /// Binds a [`f32`] to the query.
+    fn bind_f32(&'a mut self, parameter: f32) -> Result<(), DataStoreError>;
+
+    /// Binds a [`f64`] to the query.
+    fn bind_f64(&'a mut self, parameter: f64) -> Result<(), DataStoreError>;
+
+    /// Binds a String to the query.
+    fn bind_string(&'a mut self, parameter: String) -> Result<(), DataStoreError>;
+
+    /// Executes the parameterized query and returns the resulting rows.
+    async fn execute(self) -> Result<Vec<U>, DataStoreError>;
 }
 
 /// Abstraction representing a single row retrieved from a data store
@@ -60,299 +106,23 @@ pub trait DataRow<T: DataVal>: Send + Sync {
 
 /// Abstraction for a data store capable of executing queries
 #[async_trait]
-pub trait DataStore<T: DataVal, U: DataRow<T>>: Clone + Send + Sync {
-    /// Executes a basic SQL statement. If any user input is required, use [`execute_parameterized_query`](Self::execute_parameterized_query)
-    async fn execute_query(&self, query: String) -> Result<Vec<U>, DataStoreError>;
+pub trait DataStore<'a, T: DataVal, U: DataRow<T>, PQ: ParameterizedQuery<'a, T, U>>:
+    Clone + Send + Sync
+{
+    /// Executes a basic SQL statement. If any user input is required, use [`init_parameterized_query`](Self::init_parameterized_query)
+    async fn execute_query(&'a self, query: &'a str) -> Result<Vec<U>, DataStoreError>;
 
-    /// Executes a parameterized query. It is expected that the query string will have sequential placeholders
-    /// for the paramterized data.
+    /// Initializes a [`ParameterizedQuery`] with the provided query statement.
     ///
-    /// Example: If passing 5 elements into the query. the query should contain `$1`, `$2`, `$3`, `$4`, and `$5`, and
+    /// Note: It is expected that the query statement will have sequential placeholders for the paramterized data.
+    /// Example: If passing 5 elements into the query, the query should contain `$1`, `$2`, `$3`, `$4`, and `$5`, and
     /// the bindings should have no fewer than 5 elements (any additional elements beyond 5 will be ignored).
-    async fn execute_parameterized_query(
-        &self,
-        query: String,
-        bindings: Vec<String>,
-    ) -> Result<Vec<U>, DataStoreError>;
-}
-
-/// A collection of prebuilt implementations of the traits in this library that are useful for unit tests.
-pub mod test_utils {
-    use super::{DataStoreError, DataVal};
-    use chrono::{DateTime, Utc};
-    use std::{
-        error::Error,
-        fmt::{self, Display, Formatter},
-    };
-
-    /// A default implementation of [`std::error::Error`] for use in test cases.
-    #[derive(Debug)]
-    pub struct TestError;
-    impl Display for TestError {
-        fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
-            write!(f, "TestError!")
-        }
-    }
-    impl Error for TestError {}
-
-    /// Implementation of [`DataVal`] that can be configured to return mock data.
-    /// Each field is optional, and the various implementations of the `DataVal` methods will attempt to read from the corresponding field.
-    /// If a field is populated, its value is returned. If it is not, an instance of [`TestError`] is generated and returned.
-    pub struct TestVal {
-        pub test_bool: Option<bool>,
-        pub test_i8: Option<i8>,
-        pub test_i16: Option<i16>,
-        pub test_i32: Option<i32>,
-        pub test_i64: Option<i64>,
-        pub test_f32: Option<f32>,
-        pub test_f64: Option<f64>,
-        pub test_string: Option<String>,
-        pub test_datetime: Option<DateTime<Utc>>,
-    }
-    impl TestVal {
-        /// Convenience method for generating an instance of [`TestVal`] with all fields set to [`None`].
-        pub fn new() -> Self {
-            Self {
-                test_bool: None,
-                test_datetime: None,
-                test_f32: None,
-                test_f64: None,
-                test_i16: None,
-                test_i32: None,
-                test_i64: None,
-                test_i8: None,
-                test_string: None,
-            }
-        }
-
-        fn translate<T>(op: Option<T>) -> Result<T, DataStoreError> {
-            op.ok_or(DataStoreError::from(
-                Box::new(TestError) as Box<dyn std::error::Error + Send + Sync>
-            ))
-        }
-    }
-    impl Default for TestVal {
-        fn default() -> Self {
-            Self::new()
-        }
-    }
-    impl DataVal for TestVal {
-        fn to_bool(self) -> Result<bool, DataStoreError> {
-            Self::translate(self.test_bool)
-        }
-
-        fn to_i8(self) -> Result<i8, DataStoreError> {
-            Self::translate(self.test_i8)
-        }
-
-        fn to_i16(self) -> Result<i16, DataStoreError> {
-            Self::translate(self.test_i16)
-        }
-
-        fn to_i32(self) -> Result<i32, DataStoreError> {
-            Self::translate(self.test_i32)
-        }
-
-        fn to_i64(self) -> Result<i64, DataStoreError> {
-            Self::translate(self.test_i64)
-        }
-
-        fn to_f32(self) -> Result<f32, DataStoreError> {
-            Self::translate(self.test_f32)
-        }
-
-        fn to_f64(self) -> Result<f64, DataStoreError> {
-            Self::translate(self.test_f64)
-        }
-
-        fn to_string(self) -> Result<String, DataStoreError> {
-            Self::translate(self.test_string)
-        }
-
-        fn to_datetime(self) -> Result<DateTime<Utc>, DataStoreError> {
-            Self::translate(self.test_datetime)
-        }
-    }
-
-    #[cfg(test)]
-    mod tests {
-        use super::*;
-
-        #[test]
-        fn test_val_to_bool() {
-            let err_val = TestVal::new();
-            assert!(err_val.to_bool().is_err());
-
-            let mut val = TestVal::new();
-            val.test_bool = Some(true);
-            assert!(val.to_bool().unwrap());
-        }
-
-        #[test]
-        fn test_val_to_i8() {
-            let err_val = TestVal::new();
-            assert!(err_val.to_i8().is_err());
-
-            let mut val = TestVal::new();
-            val.test_i8 = Some(0_i8);
-            assert_eq!(0_i8, val.to_i8().unwrap());
-        }
-
-        #[test]
-        fn test_val_to_i16() {
-            let err_val = TestVal::new();
-            assert!(err_val.to_i16().is_err());
-
-            let mut val = TestVal::new();
-            val.test_i16 = Some(0_i16);
-            assert_eq!(0_i16, val.to_i16().unwrap());
-        }
-
-        #[test]
-        fn test_val_to_i32() {
-            let err_val = TestVal::new();
-            assert!(err_val.to_i32().is_err());
-
-            let mut val = TestVal::new();
-            val.test_i32 = Some(0_i32);
-            assert_eq!(0_i32, val.to_i32().unwrap());
-        }
-
-        #[test]
-        fn test_val_to_i64() {
-            let err_val = TestVal::new();
-            assert!(err_val.to_i64().is_err());
-
-            let mut val = TestVal::new();
-            val.test_i64 = Some(0_i64);
-            assert_eq!(0_i64, val.to_i64().unwrap());
-        }
-
-        #[test]
-        fn test_val_to_f32() {
-            let err_val = TestVal::new();
-            assert!(err_val.to_f32().is_err());
-
-            let mut val = TestVal::new();
-            val.test_f32 = Some(0_f32);
-            assert_eq!(0_f32, val.to_f32().unwrap());
-        }
-
-        #[test]
-        fn test_val_to_f64() {
-            let err_val = TestVal::new();
-            assert!(err_val.to_f64().is_err());
-
-            let mut val = TestVal::new();
-            val.test_f64 = Some(0_f64);
-            assert_eq!(0_f64, val.to_f64().unwrap());
-        }
-
-        #[test]
-        fn test_val_to_string() {
-            let err_val = TestVal::new();
-            assert!(err_val.to_string().is_err());
-
-            let mut val = TestVal::new();
-            val.test_string = Some(String::default());
-            assert_eq!(String::default(), val.to_string().unwrap());
-        }
-
-        #[test]
-        fn test_val_to_datetime() {
-            let err_val = TestVal::new();
-            assert!(err_val.to_datetime().is_err());
-
-            let mut val = TestVal::new();
-            let now = Utc::now();
-            val.test_datetime = Some(now.clone());
-            assert_eq!(now, val.to_datetime().unwrap());
-        }
-    }
+    fn init_parameterized_query(&'a self, query: &'a str) -> PQ;
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
-    use test_utils::TestVal;
-
-    #[derive(Debug)]
-    struct DummyRow {
-        data: String,
-    }
-    impl DataRow<TestVal> for DummyRow {
-        fn get(&self, _: &str) -> TestVal {
-            let mut val = TestVal::new();
-            val.test_string = Some(self.data.clone());
-            val
-        }
-    }
-    impl Clone for DummyRow {
-        fn clone(&self) -> Self {
-            DummyRow {
-                data: self.data.clone(),
-            }
-        }
-    }
-    impl PartialEq for DummyRow {
-        fn eq(&self, other: &Self) -> bool {
-            self.data == other.data
-        }
-    }
-
-    struct DummyDataStore {
-        data: Vec<DummyRow>,
-    }
-    #[async_trait]
-    impl DataStore<TestVal, DummyRow> for DummyDataStore {
-        async fn execute_query(&self, _: String) -> Result<Vec<DummyRow>, DataStoreError> {
-            Ok(self.data.clone())
-        }
-        async fn execute_parameterized_query(
-            &self,
-            _: String,
-            _: Vec<String>,
-        ) -> Result<Vec<DummyRow>, DataStoreError> {
-            Ok(self.data.clone())
-        }
-    }
-    impl Clone for DummyDataStore {
-        fn clone(&self) -> Self {
-            Self {
-                data: self.data.clone(),
-            }
-        }
-    }
-
-    #[tokio::test]
-    async fn test_dummy_data_store() {
-        let data1 = DummyRow {
-            data: "row1".to_string(),
-        };
-        let data2 = DummyRow {
-            data: "row2".to_string(),
-        };
-        let store = DummyDataStore {
-            data: vec![data1, data2],
-        };
-        let results = store
-            .execute_query("SELECT * FROM dummy".to_string())
-            .await
-            .unwrap();
-        assert_eq!(results.len(), 2);
-        let expected = results[0].get("").to_string();
-        assert_eq!("row1".to_string(), expected.unwrap());
-        assert_eq!(store.data, store.clone().data);
-
-        let parameterized_results = store
-            .execute_parameterized_query(
-                "SELECT * FROM dummy".to_string(),
-                vec!["Some binding".to_string()],
-            )
-            .await
-            .unwrap();
-        assert_eq!(parameterized_results, results);
-    }
 
     #[test]
     fn test_display_datastore_error() {
--- src/postgres/mod.rs
+++ src/postgres/mod.rs
@@ -1,14 +1,16 @@
-use super::{DataRow, DataStore, DataStoreError, DataVal};
+use super::{DataRow, DataStore, DataStoreError, DataVal, ParameterizedQuery};
 use async_trait::async_trait;
+use chrono::{DateTime, Utc};
+use rust_env_var_lib::env_var;
 use sqlx::{
     Decode, Error, Postgres, Row, Value, ValueRef,
-    postgres::{PgConnectOptions, PgPool, PgPoolOptions, PgRow, PgValue, PgValueRef},
+    postgres::{PgArguments, PgConnectOptions, PgPool, PgPoolOptions, PgRow, PgValue, PgValueRef},
+    query::Query,
 };
-use std::{env, time::Duration};
+use std::time::Duration;
 use tracing::error;
 
-const FAILED_CONVERSION_MSG: &str =
-    "Failed to convert the results to the desired type. See error log for details";
+const FAILED_CONVERSION_MSG: &str = "Invalid data conversion. See error log for details";
 impl From<Box<dyn std::error::Error + Send + Sync>> for DataStoreError {
     fn from(err: Box<dyn std::error::Error + Send + Sync>) -> Self {
         error!("{:?}", err);
@@ -28,50 +30,6 @@ impl From<Error> for DataStoreError {
     }
 }
 
-/// Postgres implementation of the [`DataStore`] trait
-pub struct PostgresDataStore {
-    db_pool: PgPool,
-}
-impl PostgresDataStore {
-    async fn establish_connection_pool() -> PgPool {
-        let host = env::var("DATABASE_HOST").expect("DATABASE_URL must be set");
-
-        let port_str = env::var("DATABASE_PORT").expect("DATABASE_PORT must be set");
-        let port: u16 = str::parse(port_str.as_str()).expect("Could not parse DB port into a u16");
-
-        let username = env::var("DATABASE_USER").expect("DATABASE_USER must be set");
-        let password = env::var("DATABASE_PASS").expect("DATABASE_PASS must be set");
-
-        let db_name = env::var("DATABASE_NAME").expect("DATABASE_NAME must be set");
-
-        let connection_config = PgConnectOptions::new()
-            .host(host.as_str())
-            .port(port)
-            .username(username.as_str())
-            .password(password.as_str())
-            .database(db_name.as_str());
-
-        PgPoolOptions::new()
-            .max_connections(5)
-            .acquire_timeout(Duration::from_secs(10))
-            .connect_with(connection_config)
-            .await
-            .expect("Failed to connect to database...")
-    }
-    pub async fn new() -> Self {
-        Self {
-            db_pool: Self::establish_connection_pool().await,
-        }
-    }
-}
-impl Clone for PostgresDataStore {
-    fn clone(&self) -> Self {
-        Self {
-            db_pool: self.db_pool.clone(),
-        }
-    }
-}
-
 /// Postgres implementation of the [`DataVal`] trait.
 pub struct PostgresDataVal {
     column_data: Result<PgValue, DataStoreError>,
@@ -83,6 +41,13 @@ impl PostgresDataVal {
 }
 impl DataVal for PostgresDataVal {
     fn to_bool(self) -> Result<bool, DataStoreError> {
+        match self.column_data {
+            Ok(value) => Self::decode(value.as_ref()),
+            Err(err) => Err(err),
+        }
+    }
+
+    fn to_datetime(self) -> Result<DateTime<Utc>, DataStoreError> {
         match self.column_data {
             Ok(value) => Self::decode(value.as_ref()),
             Err(err) => Err(err),
@@ -137,13 +102,6 @@ impl DataVal for PostgresDataVal {
             Err(err) => Err(err),
         }
     }
-
-    fn to_datetime(self) -> Result<chrono::DateTime<chrono::Utc>, DataStoreError> {
-        match self.column_data {
-            Ok(value) => Self::decode(value.as_ref()),
-            Err(err) => Err(err),
-        }
-    }
 }
 
 /// Represents a single row retrieved from a Postgres database
@@ -167,40 +125,148 @@ impl DataRow<PostgresDataVal> for PostgresDataRow {
     }
 }
 
-/// Encapsulates the execution of queries against a Postgres database.
-/// Returns results as [`PostgresDataRow`] instances.
+pub struct PostgresParameterizedQuery<'a> {
+    query_builder: Query<'a, Postgres, PgArguments>,
+    db_pool: &'a PgPool,
+}
 #[async_trait]
-impl DataStore<PostgresDataVal, PostgresDataRow> for PostgresDataStore {
-    async fn execute_query(&self, query: String) -> Result<Vec<PostgresDataRow>, DataStoreError> {
-        let query_result = sqlx::query(query.as_str()).fetch_all(&self.db_pool).await;
+impl<'a> ParameterizedQuery<'a, PostgresDataVal, PostgresDataRow>
+    for PostgresParameterizedQuery<'a>
+{
+    fn bind_bool(&'a mut self, parameter: bool) -> Result<(), DataStoreError> {
+        self.query_builder
+            .try_bind(parameter)
+            .map_err(DataStoreError::from)
+    }
+
+    fn bind_datetime(&'a mut self, parameter: DateTime<Utc>) -> Result<(), DataStoreError> {
+        self.query_builder
+            .try_bind(parameter)
+            .map_err(DataStoreError::from)
+    }
+
+    fn bind_i8(&'a mut self, parameter: i8) -> Result<(), DataStoreError> {
+        self.query_builder
+            .try_bind(parameter)
+            .map_err(DataStoreError::from)
+    }
+
+    fn bind_i16(&'a mut self, parameter: i16) -> Result<(), DataStoreError> {
+        self.query_builder
+            .try_bind(parameter)
+            .map_err(DataStoreError::from)
+    }
+
+    fn bind_i32(&'a mut self, parameter: i32) -> Result<(), DataStoreError> {
+        self.query_builder
+            .try_bind(parameter)
+            .map_err(DataStoreError::from)
+    }
+
+    fn bind_i64(&'a mut self, parameter: i64) -> Result<(), DataStoreError> {
+        self.query_builder
+            .try_bind(parameter)
+            .map_err(DataStoreError::from)
+    }
+
+    fn bind_f32(&'a mut self, parameter: f32) -> Result<(), DataStoreError> {
+        self.query_builder
+            .try_bind(parameter)
+            .map_err(DataStoreError::from)
+    }
+
+    fn bind_f64(&'a mut self, parameter: f64) -> Result<(), DataStoreError> {
+        self.query_builder
+            .try_bind(parameter)
+            .map_err(DataStoreError::from)
+    }
+
+    fn bind_string(&'a mut self, parameter: String) -> Result<(), DataStoreError> {
+        self.query_builder
+            .try_bind(parameter)
+            .map_err(DataStoreError::from)
+    }
+
+    async fn execute(self) -> Result<Vec<PostgresDataRow>, DataStoreError> {
+        let query_result = self.query_builder.fetch_all(self.db_pool).await;
         match query_result {
             Ok(rows) => Ok(rows.into_iter().map(PostgresDataRow::from).collect()),
-            Err(e) => {
-                error!("Query failed: {}", e);
-                Err(DataStoreError {
-                    details: "Query execution failed. See system logs for details.".to_string(),
-                })
-            }
+            Err(e) => Err(DataStoreError::from(e)),
         }
     }
-    async fn execute_parameterized_query(
-        &self,
-        query: String,
-        bindings: Vec<String>,
-    ) -> Result<Vec<PostgresDataRow>, DataStoreError> {
-        let mut query_builder = sqlx::query(query.as_str());
-        for binding in bindings {
-            query_builder = query_builder.bind(binding);
+}
+
+/// Postgres implementation of the [`DataStore`] trait
+pub struct PostgresDataStore {
+    db_pool: PgPool,
+}
+impl PostgresDataStore {
+    async fn establish_connection_pool() -> PgPool {
+        let host: String = env_var::get("DATABASE_HOST")
+            .to_option()
+            .expect("DATABASE_HOST must be set");
+
+        let port = env_var::get("DATABASE_PORT").or(5432_u16);
+
+        let username: String = env_var::get("DATABASE_USER")
+            .to_option()
+            .expect("DATABASE_USER must be set");
+        let password: String = env_var::get("DATABASE_PASS")
+            .to_option()
+            .expect("DATABASE_PASS must be set");
+
+        let db_name: String = env_var::get("DATABASE_NAME")
+            .to_option()
+            .expect("DATABASE_NAME must be set");
+
+        let connection_config = PgConnectOptions::new()
+            .host(host.as_str())
+            .port(port)
+            .username(username.as_str())
+            .password(password.as_str())
+            .database(db_name.as_str());
+
+        PgPoolOptions::new()
+            .max_connections(5)
+            .acquire_timeout(Duration::from_secs(10))
+            .connect_with(connection_config)
+            .await
+            .expect("Failed to connect to database...")
+    }
+
+    /// Creates a new instance of the `PostgresDataStore` with an established connection pool.
+    pub async fn new() -> Self {
+        Self {
+            db_pool: Self::establish_connection_pool().await,
         }
-        let query_result = query_builder.fetch_all(&self.db_pool).await;
+    }
+}
+impl Clone for PostgresDataStore {
+    fn clone(&self) -> Self {
+        Self {
+            db_pool: self.db_pool.clone(),
+        }
+    }
+}
+#[async_trait]
+impl<'a> DataStore<'a, PostgresDataVal, PostgresDataRow, PostgresParameterizedQuery<'a>>
+    for PostgresDataStore
+{
+    async fn execute_query(
+        &'a self,
+        query: &'a str,
+    ) -> Result<Vec<PostgresDataRow>, DataStoreError> {
+        let query_result = sqlx::query(query).fetch_all(&self.db_pool).await;
         match query_result {
             Ok(rows) => Ok(rows.into_iter().map(PostgresDataRow::from).collect()),
-            Err(e) => {
-                error!("Query failed: {}", e);
-                Err(DataStoreError {
-                    details: "Query execution failed. See system logs for details.".to_string(),
-                })
-            }
+            Err(e) => Err(DataStoreError::from(e)),
+        }
+    }
+    fn init_parameterized_query(&'a self, query: &'a str) -> PostgresParameterizedQuery<'a> {
+        let query_builder = sqlx::query(query);
+        PostgresParameterizedQuery {
+            query_builder,
+            db_pool: &self.db_pool,
         }
     }
 }
